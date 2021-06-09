7e7f9b4358838eacd7f093717ca0ffbff15981bf
Danique <danique.montanus@gmail.com>
2021-06-09 18:11:42 +0200

    Titel van een film word nu uitgelezen vanaf een API, dit alleen nog lat en zien in de recyclerview.

--- app/src/main/java/com/example/movieserieswiperandroidapp/MainActivity.java
+++ app/src/main/java/com/example/movieserieswiperandroidapp/MainActivity.java
@@ -5,12 +5,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter recyclerViewAdapter;
     private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,28 @@ public class MainActivity extends AppCompatActivity {
         final Movie[] movies = new Movie[2];
         movies[0] = new Movie("Dark night", 250, "bla bla bla", 1);
         movies[1] = new Movie("Dark night 2", 350, "bla bla bla bla bla", 2);
+
+        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://api.themoviedb.org/3/movie/11?api_key=c90281a1d17bd06cb743a01a02f7c620", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("Title: ", response.getString("original_title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener(){
+           @Override
+           public void onErrorResponse(VolleyError error){
+            Log.d("niet gelukt", error.getMessage());
+        }
+        });
+
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+
 
         recyclerViewAdapter = new MovieAdapter(movies);
         recyclerView.setAdapter(recyclerViewAdapter);
