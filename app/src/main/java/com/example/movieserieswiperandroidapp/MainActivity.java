5ecca650fb3bc15fc2e36e7df667edfd56f45167
Danique <danique.montanus@gmail.com>
2021-06-30 16:59:53 +0200

    Werkende swiper, met data uit onze eigen database!

--- app/src/main/java/com/example/movieserieswiperandroidapp/MainActivity.java
+++ app/src/main/java/com/example/movieserieswiperandroidapp/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.movieserieswiperandroidapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DiffUtil;
 
+import android.content.ClipData;
+import android.graphics.Movie;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeableMethod;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,16 +34,13 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mList;
+    private static final String TAG = "MainActivity";
+    private CardStackLayoutManager manager;
+    public CardStackAdapter adapter;
 
-    private LinearLayoutManager linearLayoutManager;
-    private DividerItemDecoration dividerItemDecoration;
-    private List<Movie> movieList;
-    private RecyclerView.Adapter adapter;
 
-    private String APIurl = "https://api.themoviedb.org/3/movie/popular?api_key=c90281a1d17bd06cb743a01a02f7c620&language=en-US";
 
 
     @Override
@@ -44,113 +48,126 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mList = findViewById(R.id.main_list);
 
-        movieList = new ArrayList<>();
-        adapter = new MovieAdapter(getApplicationContext(), movieList);
 
-        linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        dividerItemDecoration = new DividerItemDecoration(mList.getContext(),
-                linearLayoutManager.getOrientation());
+        CardStackView cardStackView = findViewById(R.id.card_stack_view);
+        manager = new CardStackLayoutManager(this, new CardStackListener() {
+            @Override
+            public void onCardDragging(Direction direction, float ratio) {
+                Log.d(TAG, "onCardDragging: d=" + direction.name() + " ratio=" + ratio);
+            }
 
-        mList.setHasFixedSize(true);
-        mList.setLayoutManager(linearLayoutManager);
-        mList.addItemDecoration(dividerItemDecoration);
-        mList.setAdapter(adapter);
+            @Override
+            public void onCardSwiped(Direction direction) {
+                Log.d(TAG, "onCardSwiped: p=" + manager.getTopPosition() + " d=" + direction);
+                if (direction == Direction.Right){
+                    Toast.makeText(MainActivity.this, "Direction Right", Toast.LENGTH_SHORT).show();
 
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
-        itemTouchHelper.attachToRecyclerView(mList);
 
-        getData();
+                }
+                if (direction == Direction.Top){
+                    Toast.makeText(MainActivity.this, "Direction Top", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Left){
+                    Toast.makeText(MainActivity.this, "Direction Left", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Bottom){
+                    Toast.makeText(MainActivity.this, "Direction Bottom", Toast.LENGTH_SHORT).show();
+                }
+
+                // Paginating
+                if (manager.getTopPosition() == adapter.getItemCount() - 5){
+                    paginate();
+                }
+            }
+
+            @Override
+            public void onCardRewound() {
+                Log.d(TAG, "onCardRewound: " + manager.getTopPosition());
+            }
+
+            @Override
+            public void onCardCanceled() {
+                Log.d(TAG, "onCardRewound: " + manager.getTopPosition());
+            }
+
+            @Override
+            public void onCardAppeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.movie_name);
+                Log.d(TAG, "onCardAppeared: " + position + ", movie name: " + tv.getText());
+            }
+
+            @Override
+            public void onCardDisappeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.movie_name);
+                Log.d(TAG, "onCardAppeared: " + position + ", movie name: " + tv.getText());
+
+
+            }
+        });
+
+        manager.setStackFrom(StackFrom.None);
+        manager.setVisibleCount(3);
+        manager.setTranslationInterval(8.0f);
+        manager.setScaleInterval(0.95f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setMaxDegree(20.0f);
+        manager.setDirections(Direction.FREEDOM);
+        manager.setCanScrollHorizontal(true);
+        manager.setSwipeableMethod(SwipeableMethod.Manual);
+        manager.setOverlayInterpolator(new LinearInterpolator());
+        adapter = new CardStackAdapter(addList());
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(adapter);
+        cardStackView.setItemAnimator(new DefaultItemAnimator());
+
+        addList();
     }
 
-    Movie deletedMovie = null;
+    private void paginate() {
+        List<ItemModel> old = adapter.getItems();
+        List<ItemModel> baru = new ArrayList<>(addList());
+        CardStackCallback callback = new CardStackCallback(old, baru);
+        DiffUtil.DiffResult hasil = DiffUtil.calculateDiff(callback);
+        adapter.setItems(baru);
+        hasil.dispatchUpdatesTo(adapter);
+    }
 
-    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT ) {
-        @Override
-        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-            return false;
-        }
-
-        @Override
-        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-            final int position = viewHolder.getAdapterPosition();
-            List<Movie> archivedMovie = new ArrayList<>();
-
-            switch (direction) {
-                case ItemTouchHelper.LEFT:
-                    deletedMovie = movieList.get(position);
-                    movieList.remove(position);
-                    adapter.notifyItemRemoved(position);
-                    /*Snackbar.make(mList, deletedMovie, Snackbar.LENGTH_LONG)
-                            .setAction("Undo", new View.OnClickListener(){
-                                @Override
-                                public void onClick(View view){
-                                    movieList.add(position, deletedMovie);
-                                    adapter.notifyItemInserted(position);
-                                }
-                            }).show();*/
-                    break;
-
-                case ItemTouchHelper.RIGHT:
-                    final Movie movieName = movieList.get(position);
-                    archivedMovie.add(movieName);
-                    movieList.remove(position);
-                    adapter.notifyItemRemoved(position);
-                    break;
-            }
-        }
-    };
-
-    private void getData(){
-
-        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
-
-        Request jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                APIurl,
-                null,
-                new Response.Listener<JSONObject>() {
+    private List<ItemModel> addList() {
+        String Movieurl = "https://movieserieswiperdb-qioab.ondigitalocean.app/api/auth/movies";
+        List<ItemModel> items = new ArrayList<>();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Movieurl,
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("results");
-                            //String movieName = (String) response.get("original_title");
-                            for(int i = 0; i < jsonArray.length(); i++){
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    public void onResponse(String response) {
+                        try{
+                            JSONArray array = new JSONArray(response);
 
-                                Movie movie = new Movie();
-                               // movie.setMovieId(jsonObject.getInt("id"));
-                                movie.setMovieName(jsonObject.getString("original_title"));
-                                movie.setMovieDescription(jsonObject.getString("overview"));
-                                movie.setMovieLanguage(jsonObject.getString("original_language"));
-                               // movie.setGenreId(jsonObject.getInt("genre_ids"));
-                                movieList.add(movie);
+                            for(int i = 0; i < array.length(); i++){
+                                JSONObject jsonObject = array.getJSONObject(i);
+                                ItemModel item = new ItemModel();
+
+                                item.setMovieName(jsonObject.getString("movieName"));
+                                item.setGenre(jsonObject.getString("genre"));
+                                items.add(item);
                                 adapter.notifyDataSetChanged();
+                                Log.d("movie response", response);
                             }
-
-
-                               /* Movie movie = new Movie();
-                                movie.setMovieName(movieName);
-                                movieList.add(movie);
-                                adapter.notifyDataSetChanged();*/
-
-                        } catch (JSONException e) {
+                        }catch(JSONException e){
                             e.printStackTrace();
                         }
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
-                    public void onErrorResponse(VolleyError error){
-                        Log.e("Volley", error.toString());
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("movie error", error.toString());
                     }
                 });
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            requestQueue.add(jsonObjectRequest);
 
-
-        }
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+        return items;
+    }
 
 }
-
